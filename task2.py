5eb7910ec61ad2d2573f6bbdaa7a30cb00fed3f7
dma <d.makarov@nnov.starttelecom.ru>
2015-06-18 12:14:56 +0300

    Добавлен help, добавлены небольшие комментарии в код, обрабатываются некоторые ошибки.

--- task2.py
+++ task2.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+import argparse
+from sys import argv
 
+#Организация работы стека
 class Stack:
+
 	def __init__(self):
 		self.items = []
 
@@ -15,21 +19,23 @@ class Stack:
 		return (self.items == [])
 
 
+#Основной класс генератора html из входного файла с выводом в другой файл
 class GeneratorHTML:
+
 	def __init__(self,input_file,output_file):
 		self.input_file=open(input_file,"r")
 		self.output_file=open(output_file,"w")
 		self.stack = Stack()
 
-
+	#Метод записи открытого тега по ключевому слову
 	def print_open_teg(self,teg):
 		return "%s<%s>\n" % ("\t"*(teg.count("\t")),teg.strip())
 
-
+	#Метод записи закрытого тега по ключевому слову
 	def print_closed_teg(self,teg):
 		return "%s</%s>\n" % ("\t"*(teg.count("\t")),teg.strip())
 
-
+	#Метод генерации html тегов по входным данным
 	def create_html(self):
 		for line in self.input_file:
 			if self.stack.is_empty():
@@ -49,6 +55,21 @@ class GeneratorHTML:
 		while not self.stack.is_empty():
 			self.output_file.write(self.print_closed_teg(self.stack.pop()))
 
-#Первый файл входящий, второй исходящий со сгенерированным кодом
-gener = GeneratorHTML("./test.txt","./test2.txt")
-gener.create_html()
+#Описание аргументов для запуска программы
+parser = argparse.ArgumentParser(conflict_handler='resolve',description="""Программа для генерирования тегов html из файла. 
+	Во входном файле указываются названия тегов	и отступы в соотвествии с вложенностью тегов. Теги указываются именами без кавычек и скобок
+	, например: title. Указания отступа выполняются табуляцией.""", epilog = '''(c)Dmitry Makarov 2015''')
+parser.add_argument('-i','--input_file',action='store', help="Входной файл из которого извлекает данные генератор html.")
+parser.add_argument('-o','--output_file',action='store', help="Выходной файл.")
+result=parser.parse_args()
+
+if len(argv) != 5:
+	parser.print_help()
+else:
+	try:
+		gener = GeneratorHTML(result.input_file,result.output_file)
+		gener.create_html()
+	except IndexError:
+		print "Неверно составлен входной файл"
+	except IOError:
+		print "Входного файла с таким именем не найдено"
