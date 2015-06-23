f102d85bded910f9ac0bd1b5fa7673a54518c625
dma <d.makarov@nnov.starttelecom.ru>
2015-06-23 14:17:36 +0300

    Поправлены конструкции try except в классе генератора

--- task2.py
+++ task2.py
@@ -27,7 +27,7 @@ class GeneratorHTML:
 			self.input_file=open(input_file,"r")
 			self.output_file=open(output_file,"w")
 			self.stack = Stack()
-		except IOError:
+		except:
 			print "Файл не найден"
 
 	#Метод записи открытого тега по ключевому слову
@@ -40,23 +40,26 @@ class GeneratorHTML:
 
 	#Метод генерации html тегов по входным данным
 	def create_html(self):
-		for line in self.input_file:
-			if self.stack.is_empty():
-				if line:
-					self.output_file.write(self.print_open_teg(line))
-					self.stack.push(line)
-			else:
-				if line.count("\t") > last_teg.count("\t"):
-					self.output_file.write(self.print_open_teg(line))
-					self.stack.push(line)
-				elif line.count("\t") <= last_teg.count("\t"):
-					while self.stack.items[-1].count("\t") >= line.count("\t") and not self.stack.is_empty():
-						self.output_file.write(self.print_closed_teg(self.stack.pop()))
-					self.output_file.write(self.print_open_teg(line))
-					self.stack.push(line)
-			last_teg = line
-		while not self.stack.is_empty():
-			self.output_file.write(self.print_closed_teg(self.stack.pop()))
+		try:
+			for line in self.input_file:
+				if self.stack.is_empty():
+					if line:
+						self.output_file.write(self.print_open_teg(line))
+						self.stack.push(line)
+				else:
+					if line.count("\t") > last_teg.count("\t"):
+						self.output_file.write(self.print_open_teg(line))
+						self.stack.push(line)
+					elif line.count("\t") <= last_teg.count("\t"):
+						while self.stack.items[-1].count("\t") >= line.count("\t") and not self.stack.is_empty():
+							self.output_file.write(self.print_closed_teg(self.stack.pop()))
+						self.output_file.write(self.print_open_teg(line))
+						self.stack.push(line)
+				last_teg = line
+			while not self.stack.is_empty():
+				self.output_file.write(self.print_closed_teg(self.stack.pop()))
+		except:
+			"Невозможно выполнить генерацию"
 
 #Описание аргументов для запуска программы
 parser = argparse.ArgumentParser(conflict_handler='resolve',description="""Программа для генерирования тегов html из файла. 
